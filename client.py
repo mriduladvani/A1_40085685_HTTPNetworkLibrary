963bf1ff0ae44652b53c2d3a520b0a05e055ebcd
mridul <mriduladvani@gmail.com>
2018-09-25 02:55:57 -0400

    Sample post request implemented

--- client.py
+++ client.py
@@ -5,15 +5,39 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (HOST, 80)
 client_socket.connect(server_address)
 
-request_header = 'GET / HTTP/1.0\r\nHost: '+HOST+'\r\n\r\n'
-client_socket.send(request_header.encode())
+params= 'username:helloworld&pass=blah\r\n'
+header= """
+POST HTTP/1.1
+Host: www.facebook.com
+Content-Type: application/x-www-form-urlencoded
+Connection: keep-alive
+"""
+content_length= str(len(params.encode('ascii')))
 
-response = ''
-while True:
-    recv = client_socket.recv(1024)
-    if not recv:
-        break
-    response += recv.decode()
+
+x= input('Which request do you want to send?')
+
+if(x=='GET'):
+    request_header = 'GET / HTTP/1.0\r\nHost: '+HOST+'\r\n\r\n'
+    client_socket.send(request_header.encode())
+
+    response = ''
+    while True:
+        recv = client_socket.recv(1024)
+        if not recv:
+            break
+        response= response+recv.decode()
+else:
+    request_header= header+content_length+params
+    client_socket.send(request_header.encode())
+
+    response = ''
+    while True:
+        recv = client_socket.recv(1024)
+        if not recv:
+            break
+        response = response + recv.decode()
+
 
 print (response)
 client_socket.close()
